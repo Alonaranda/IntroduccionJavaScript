b5aacd9be27b79b5ffbad289738ad1d51ab3353e
christian.alonso <christian.alonso@bbva.com>
2020-05-25 15:03:33 -0500

    Primera parte

---
+++ JS/arreglos.js
@@ -0,0 +1,20 @@
+var frutas = ['Mango', 'melon', 'Papaya'];
+
+console.log(frutas);
+console.log(frutas[1]);
+
+var semana = ['lunes', 'martes', 'miercoles',' jueves', 'viernes', 'sabado','domingo'];
+console.log(semana);
+console.log(semana.length);
+//Agregar elementos
+semana.push('Monday');
+console.log(semana);
+//Eliminar el ultimo elemento
+semana.pop();
+console.log(semana);
+var animales = ['Bufalo', 'Leon', 'Antilope', 'Cabra'];
+//ordenar alfabeticamente
+animales.sort();
+console.log(animales);
+animales.reverse();
+console.log(animales);
---
+++ JS/variables.js
@@ -0,0 +1,11 @@
+var fruta = "naranja";
+console.log(`Mi fruta es ${fruta}`);
+let num = 5;
+const a = 10;
+console.log(`Variable let: ${num} y variable const ${a}`);
+
+let multiplicacion = num * a ;
+
+console.log(multiplicacion);
+console.log(`Al dividir ${a} entre ${num} nos da un resultado de: ${a/num}`);
+
