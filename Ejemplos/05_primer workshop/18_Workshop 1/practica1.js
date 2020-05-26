5880a6104c2b6c7dda2c11ca35791b705bbbcadf
christian.alonso <christian.alonso@bbva.com>
2020-05-26 12:15:20 -0500

    Workshop1

---
+++ Ejemplos/05_primer workshop/18_Workshop 1/practica1.js
@@ -0,0 +1,11 @@
+
+
+function convertirYenes(){
+    var tipodeCambio = 99.2
+    var montoaCambiar = document.getElementById("dolares").value 
+
+    resultado = montoaCambiar * tipodeCambio
+
+    alert(resultado)
+}
+
