b672e78704b7870df2658fb51de97a4eca6d849e
christian.alonso <christian.alonso@bbva.com>
2020-05-26 09:54:46 -0500

    Primer WorkShop

---
+++ Ejemplos/05_primer workshop/17_eventos del mouse/mouseEvents.js
@@ -0,0 +1,10 @@
+function MostrarInfoMouseOver(){
+    console.log('Estoy sobre la imagen')
+}
+
+function MostrarInfoClick(){
+    console.log('Di Click')
+}
+
+MostrarInfoMouseOver()
+MostrarInfoClick()
---
+++ JS/funciones.js
@@ -0,0 +1,33 @@
+var num1 = 1000
+var num2 = 7
+var resultado = 0
+
+
+function saludar(){
+    console.log('Buen día, saludo desde una función')
+    document.write('Hola')
+}
+
+function suma(){
+    console.log(resultado = num1 + num2)
+}
+
+function convertidor(){
+    var dolaresTotales  = 100
+    var resultado = dolaresTotales * 98.5
+
+    document.write(resultado)
+}
+
+function multiplicacion(numero1){
+    var mult = numero1 * 112345
+    console.log(mult)
+}
+
+saludar()
+suma()
+convertidor()
+multiplicacion(3)
+
+
+
